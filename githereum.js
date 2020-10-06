f296ab75825ab7e0396d6bea7760f02736ad6e27
Alex Speller <alex@alexspeller.com>
2020-10-07 00:33:47 +0100

    Update githereum.js
    Remove log line in inner loop

--- githereum.js
+++ githereum.js
@@ -359,7 +359,6 @@ class Githereum {
 
     let { filename, packfile } = await this.makePackFile(async () => {
       for (let commit of commits) {
-        this.log(`Storing commit ${commit.oid}`);
         await this.writeToPackfile(commit.oid);
         await this.storeTree(commit.tree);
       }
